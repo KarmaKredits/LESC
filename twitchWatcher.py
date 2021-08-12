a814c8b2364c41be57c7e920fd10a410898651f7
KarmaKredits <karmakredits@gmail.com>
2021-08-11 20:38:12 -0400

    clean up and organization
    organization of twitch calls

--- twitchWatcher.py
+++ twitchWatcher.py
@@ -1,60 +1,64 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import os
-# chrome_options = Options()
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--disable-dev-shm-usage')
-
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
-PATH = os.getenv(key='PATHWEBDRIVER')
 
+CLIENT_ID = os.getenv(key='TWITCH_CLIENT_ID')
+SECRET  = os.getenv(key='TWITCH_SECRET')
 
 # driver = webdriver.Chrome(options=chrome_options)
 # driver.get("https://youtube.com")
-import requests
+streamerlist = ['gingersoccermom','ragdoll139','itsruddy','theleakygiraffe','midoriin4k','soundsofthewild']
+game_name = 'Rocket League'
+key_words = ['LESC','League of Extraordinary Soccer Cars']
 
-URL = 'https://api.twitch.tv/helix/streams?user_login=shwa_zee'
-chan = 'https://api.twitch.tv/helix/channels?broadcaster_id='
-stream = 'https://api.twitch.tv/helix/streams?'
-user = 'https://api.twitch.tv/helix/users?login=shwa_zee'
+streamsUrl = 'https://api.twitch.tv/helix/streams?user_login=' #login name
+channelsUrl = 'https://api.twitch.tv/helix/channels?broadcaster_id=' # ID #
+usersUrl = 'https://api.twitch.tv/helix/users?login=' #login_name
+
 authURL = 'https://id.twitch.tv/oauth2/token'
-Client_ID = os.getenv(key='TWITCH_CLIENT_ID')
-Secret  = os.getenv(key='TWITCH_SECRET')
-
-AutParams = {'client_id': Client_ID,
-             'client_secret': Secret,
+AutParams = {'client_id': CLIENT_ID,
+             'client_secret': SECRET,
              'grant_type': 'client_credentials'
              }
+access_token = None
+head = None
+
+# HTTP Code	Meaning
+# 200	Channel/Stream returned successfully
+# 400	Missing Query Parameter
+# 500	Internal Server Error; Failed to get channel information
 
 
-def Check():
+def getToken():
     AutCall = requests.post(url=authURL, params=AutParams)
+    print(autCall)
+    global access_token
     access_token = AutCall.json()['access_token']
-    print(AutCall)
+    global head
     head = {
-    'Client-ID' : Client_ID,
+    'Client-ID' : CLIENT_ID,
     'Authorization' :  "Bearer " + access_token
     }
 
-    r = requests.get(URL, headers = head).json()['data']
-    id =requests.get(user, headers = head).json()['data'][0]['id']
+def getStreamsFromTitle(phrase):
+    streams = requests.get(streamsUrl + phrase, headers = head).json()['data']
+    return streams
+def getStreamsFromLogin(login_name):
+    streams = requests.get(streamsUrl + login_name, headers = head).json()['data']
+    return streams #list of matches
 
-    r2 = requests.get(chan + str(id), headers = head).json()['data'][0]['title']
-    print(r2)
+def getUserIDFromLogin(login_name):
+    id =requests.get(usersUrl, headers = head).json()['data'][0]['id']
+    return str(id) #broadcaster_id of login name
 
-    if r:
-        r = r[0]
-        if r['type'] == 'live':
-            return True
-        else:
-            return False
-    else:
-        return False
+def getChannelFromUserID(user_id):
+    channel_info = requests.get(channelsUrl + str(user_id), headers = head).json()['data'][0]
+    return channel_info #channel info dict
 
-# import twitch
-# client = twitch.TwitchHelix(client_id=Client_ID, client_secret=Secret, scopes=[twitch.constants.OAUTH_SCOPE_ANALYTICS_READ_EXTENSIONS])
-# client.get_oauth()
-# client.get_streams()
-print(Check())
+
+if __name__ == '__main__':
+    getToken()
+    testgame='Rocket League'
+    testTitle='rank'
