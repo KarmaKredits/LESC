047d22efef67ef268cbd5a27c9f60d667943e6c9
KarmaKredits <karmakredits@gmail.com>
2022-03-31 00:18:55 -0400

    twitch to discord

--- twitchWatcher.py
+++ twitchWatcher.py
@@ -21,7 +21,7 @@ streamDiscordId = {
     'shwa_zee': 169641021519560707, 'kylure': 182367682811658240,
     'karmakredits': 174714475113480192, 'og_grips': 346715426241380362,
     'csmith_games': 492497679570436117, 'xxvhpxx': 335887314159140866,
-    'qs3v3n': 192007340927614976
+    'qs3v3n': 192007340927614976, 'altihex3': 505797270164340788
     }
 # streamDiscordId= {'karmakredits': '174714475113480192'} #test with my id
 # driver = webdriver.Chrome(options=chrome_options)
