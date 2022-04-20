e4d5396bd25b962bbb59127da0045b975586cb48
KarmaKredits <karmakredits@gmail.com>
2022-04-19 22:34:21 -0400

    sub list command

--- bot.py
+++ bot.py
@@ -23,11 +23,11 @@ intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
 # print('intents: ',intents)
-bot = discord.Client(intents=intents)
+# bot = discord.Client(intents=intents)
 help_command = commands.DefaultHelpCommand(
     no_category = 'Commands'
 )
-client = commands.Bot(command_prefix = '.', help_command=help_command)
+client = commands.Bot(command_prefix = '.', help_command=help_command,intents=intents)
 # client.intents = intents
 # client.Intents.members = True
 lescTitle='The League of Extraordinary Soccer Cars'
@@ -93,7 +93,7 @@ async def on_ready():
     global LESC3_DB
     try:
         step = 'updateFromGoogleSheets'
-        response = await updateFromGoogleSheets() #temp
+        # response = await updateFromGoogleSheets() #temp
     except Exception as e:
         print('db from redis')
         # LESC3_DB = rc.getValue('lesc3_db') #LESC1
@@ -237,11 +237,18 @@ async def on_ready():
         await msg.edit(content=newcontent)
         raise
 
+
+
+
     print('Bot Ready')
     print('==========================')
     # twitchAlerts()
     print('execute cycle from ready')
     task1 = asyncio.create_task(cycle(10))
+
+
+
+
 
 
 
@@ -276,6 +283,47 @@ async def update(ctx):
         await msg.edit(content=response)
 
 
+@client.command(brief='List available subs',
+    aliases=['subs,substitute'])
+async def sub(ctx):
+    sub_role = [963617395874889740, #sub
+    183800165767970820] #life guard
+    rank_roles = [
+    869528040265363456, #grand champ
+    869527980035153940, #champ
+    869527795456442368, #diamond
+    869527569760923658, #plat
+    869527452257517589, # gold
+    869527391351996417, #silver
+    869527337702666242, #bronze
+    # 892260316224839680, #freestyle
+    695490219687804928, #poke
+    843196839057948722 #party
+    ]
+    for guild in client.guilds:
+        sub_list = {}
+        for roleId in sub_role:
+            if guild.get_role(roleId) != None:
+                for member in guild.get_role(roleId).members:
+                    found = False
+                    for rankId in rank_roles:
+                        if found: break
+                        for memRole in member.roles:
+                            if found: break
+                            if rankId == memRole.id:
+                                print('found', rankId, memRole.name)
+                                found = True
+                                if rankId not in sub_list:
+                                    sub_list[rankId] = []
+                                sub_list[rankId].append(member.mention)
+    print(sub_list)
+    text = ''
+    embedTitle='LESC Season Substitutes'
+    embedVar = discord.Embed(title=embedTitle, color=0xffffff)
+
+    for item in sub_list:
+        embedVar.add_field(name=guild.get_role(item).name, value='\n'.join(sub_list[item]), inline=True)
+    await ctx.send(embed=embedVar)
 
 @client.command(brief='Check bot latency')
 async def ping(ctx):
